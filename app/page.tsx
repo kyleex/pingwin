9262d005d41b2f9d7d1c2232ef6e162c71198d37
kyleex <antwill34@icloud.com>
2024-05-08 23:15:21 +0200

    feat: Add login page and authentication layout
    The code changes include adding a login page component (`app/auth/login/page.tsx`) and an authentication layout component (`app/auth/layout.tsx`). These components are necessary for implementing the authentication flow in the application.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
-import { auth } from "@/src/lib/auth";
-import { LoginButton, LogoutButton } from "./authButtons";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
 
 export default async function Home() {
-  const session = await auth();
   return (
-    <div>
-      <h1>
-        {session?.user
-          ? "Authentificated " + session?.user.email
-          : "Not Authentificated"}
-      </h1>
-      <div>{!session?.user ? <LoginButton /> : <LogoutButton />}</div>
-    </div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentification service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
