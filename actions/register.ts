f892ca000b416472f6a9127cc0eda848f445801b
kyleex <antwill34@icloud.com>
2024-05-19 19:36:11 +0200

    feat: implemented register (create user in db)
    next step : able to login if account created

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,13 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
 import { registerSchema } from "@/schemas";
+import { get } from "http";
+
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
   const validatedFields = registerSchema.safeParse(values);
@@ -11,5 +16,24 @@ export const register = async (values: z.infer<typeof registerSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created" };
 };
---
+++ data/user.ts
@@ -0,0 +1,26 @@
+import { db } from "@/lib/db";
+import email from "next-auth/providers/email";
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  } catch {
+    return null;
+  }
+};
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  } catch {
+    return null;
+  }
+};
